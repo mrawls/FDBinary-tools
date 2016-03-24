3face5f869b80eb0b0689513788a1a734f0c048d
Meredith Rawls <meredith.rawls@gmail.com>
2016-03-23 19:04:09 -0700

    assumes a single input spectrum; choice btwn diff/percent diff

--- activity_plotter.py
+++ activity_plotter.py
@@ -11,13 +11,47 @@ Takes a model spectrum and an observed spectrum
 Subtracts them at wavelengths you define to look for signatures of magnetic activity.
 '''
 
-s1file = '../../FDBinary/9246715/FDBinary_star1_bluer.fits' # star 1
-s2file = '../../FDBinary/9246715/FDBinary_star2_bluer.fits' # star 2
-#model = '../../FDBinary/9246715/model_rg48.bf.arces.txt'
-model = '../../RG_spectra/model_rg50_30_R50000.txt'
 
-s1file2 = '../../FDBinary/9246715/FDBinary_star1_caII.fits'
-s2file2 = '../../FDBinary/9246715/FDBinary_star2_caII.fits'
+# 8702921 --> DONE
+#s1file = '../../RG_spectra/8702921/8702921_combine2.fits'
+#model =  '../../RG_spectra/coelho2014_spectra/norm_t05000_g+3.0_p02p00_hrplc.fits'
+#AdjustContinuum = False
+
+# 9291629 --> NEEDS DOING WITH BEST INPUT FD3 PARAMETERS FROM FINAL ELC FIT
+s1file = '../../FDBinary/9291629/FDBinaryRG_9291629v4.fits'
+model =  '../../RG_spectra/coelho2014_spectra/norm_t04750_g+3.0_p00p00_hrplc.fits'
+AdjustContinuum = True; offset = -0.05
+
+# 3955867 --> DONE
+#s1file = '../../FDBinary/3955867/FDBinaryRG_3955867v4.fits'
+#model =  '../../RG_spectra/coelho2014_spectra/norm_t05000_g+3.0_m05p00_hrplc.fits'
+#AdjustContinuum = True; offset = -0.05
+
+# 10001167 --> DONE
+#s1file = '../../FDBinary/10001167/FDBinaryRG_10001167v3.fits'
+#model =  '../../RG_spectra/coelho2014_spectra/norm_t04750_g+2.5_m08p04_hrplc.fits'
+## model with m05p00 looks very similar
+#AdjustContinuum = True; offset = -0.02
+
+# 5786154 --> DONE
+#s1file = '../../FDBinary/5786154/FDBinaryRG_5786154v3.fits'
+#model =  '../../RG_spectra/coelho2014_spectra/norm_t04750_g+2.5_p00p00_hrplc.fits'
+#AdjustContinuum = True; offset = -0.05
+
+# 7037405 --> DONE
+#s1file = '../../FDBinary/7037405/FDBinaryRG_7037405v4.fits'
+#model =  '../../RG_spectra/coelho2014_spectra/norm_t04500_g+2.5_m05p00_hrplc.fits'
+#AdjustContinuum = False
+
+# 9970396 --> NEEDS DOING WITH BEST INPUT FD3 PARAMETERS FROM FINAL ELC FIT
+#s1file = '../../FDBinary/9970396/FDBinaryRG_9970396v3.fits'
+#model =  '../../RG_spectra/coelho2014_spectra/norm_t05000_g+3.0_p00p00_hrplc.fits'
+#AdjustContinuum = True; offset = -0.05
+
+s1file2 = s1file
+#s2file = '../../FDBinary/9246715/FDBinary_star2_bluer.fits' # star 2
+#model = '../../FDBinary/9246715/model_rg48.bf.arces.txt' # used for 9246715
+#s2file2 = '../../FDBinary/9246715/FDBinary_star2_caII.fits'
 
 red = '#e34a33'
 yel = '#fdbb84'
@@ -60,22 +94,22 @@ def activity_plot(fignum, flux, color):
     Uses plot_chunk for a specific set of manually labeled wavelength regions
     '''
     global fm
-    #diff = flux - fm
-    diff = (fm - flux)/flux
+    diff = flux - fm       # REGULAR SUBTRACTION
+    #diff = (fm - flux)/flux # PERCENT DIFFERENCE
     fig = plt.figure(fignum, figsize=(18,8))
     plt.subplots_adjust(hspace=0.35)
     fig.text(0.5, 0.03, r'Wavelength (\AA)', ha='center', va='center', size=24)
     fig.text(0.07, 0.5, 'Scaled Flux', ha='center', va='center', size=24, rotation='vertical')
      
-     ax = plt.subplot(2,5,1)
-     #ax.get_yaxis().set_ticklabels([])
-     plt.title('$\hbox{Fe\kern 0.1em{\sc i}}$, mag', size=24)
-     line = 5557.913
-     ax.axvline(x=line, color='0.75')
-     ax.xaxis.set_major_locator(MaxNLocator(4))
-     plot_chunk(flux, diff, int(line-5), int(line+5), color)
+    ax = plt.subplot(2,5,1)
+    #ax.get_yaxis().set_ticklabels([])
+    plt.title('$\hbox{Fe\kern 0.1em{\sc i}}$, mag', size=24)
+    line = 5557.913
+    ax.axvline(x=line, color='0.75')
+    ax.xaxis.set_major_locator(MaxNLocator(4))
+    plot_chunk(flux, diff, int(line-5), int(line+5), color)
 
-     ax = plt.subplot(2,5,2)
+    ax = plt.subplot(2,5,2)
     ax.get_yaxis().set_ticklabels([])
     plt.title('$\hbox{Fe\kern 0.1em{\sc i}}$, non-mag', size=24)
     line = 5576.099
@@ -83,13 +117,13 @@ def activity_plot(fignum, flux, color):
     ax.xaxis.set_major_locator(MaxNLocator(4))
     plot_chunk(flux, diff, int(line-5), int(line+5), color)
  
-     ax = plt.subplot(2,5,3)
-     ax.get_yaxis().set_ticklabels([])
-     plt.title('$\hbox{Fe\kern 0.1em{\sc i}}$, non-mag', size=24)
-     line = 5691.505
-     ax.axvline(x=line, color='0.75')
-     ax.xaxis.set_major_locator(MaxNLocator(4))
-     plot_chunk(flux, diff, int(line-5), int(line+5), color)
+    ax = plt.subplot(2,5,3)
+    ax.get_yaxis().set_ticklabels([])
+    plt.title('$\hbox{Fe\kern 0.1em{\sc i}}$, non-mag', size=24)
+    line = 5691.505
+    ax.axvline(x=line, color='0.75')
+    ax.xaxis.set_major_locator(MaxNLocator(4))
+    plot_chunk(flux, diff, int(line-5), int(line+5), color)
 
     ax = plt.subplot(2,5,4)
     ax.get_yaxis().set_ticklabels([])
@@ -128,8 +162,13 @@ def activity_plot(fignum, flux, color):
 # Interpolate when necessary and make plots
 
 w1, f1 = readImage(s1file)
-w2, f2 = readImage(s2file)
-wm, fm = np.loadtxt(model, unpack=True)
+if AdjustContinuum == True:
+    f1 = f1 + offset
+#w2, f2 = readImage(s2file)
+if model[-4:] == 'fits':
+    wm, fm = readImage(model)
+else:
+    wm, fm = np.loadtxt(model, unpack=True)
 
 inds = np.where( (wm > w1[0]) & (wm < w1[-1]) )[0]
 fm = fm[inds]
@@ -137,18 +176,23 @@ wm = wm[inds]
 
 fit1 = interp1d(w1, f1)
 f1 = fit1(wm)
-fit2 = interp1d(w2, f2)
-f2 = fit2(wm)
+#fit2 = interp1d(w2, f2)
+#f2 = fit2(wm)
 
 activity_plot(1, f1, red)
-activity_plot(2, f2, yel)
+#activity_plot(2, f2, yel)
 
 
 # Repeat the process above more explicitly for the Ca II wavelength region
 
 w1, f1 = readImage(s1file2)
-w2, f2 = readImage(s2file2)
-wm, fm = np.loadtxt(model, unpack=True)
+if AdjustContinuum == True:
+    f1 = f1 + offset
+#w2, f2 = readImage(s2file2)
+if model[-4:] == 'fits':
+    wm, fm = readImage(model)
+else:
+    wm, fm = np.loadtxt(model, unpack=True)
 
 inds = np.where( (wm > w1[0]) & (wm < w1[-1]) )[0]
 fm = fm[inds]
@@ -156,11 +200,11 @@ wm = wm[inds]
 
 fit1 = interp1d(w1, f1)
 f1 = fit1(wm)
-fit2 = interp1d(w2, f2)
-f2 = fit2(wm)
+#fit2 = interp1d(w2, f2)
+#f2 = fit2(wm)
 
 # Star 1
-#diff = f1 - fm
+diff = f1 - fm
 diff = (fm - f1)/f1
 plt.figure(1, figsize=(18,8))
 ax = plt.subplot2grid((2,5), (1,2), colspan=2)
@@ -168,20 +212,20 @@ ax.get_yaxis().set_ticklabels([])
 plt.title('$\hbox{Ca\kern 0.1em{\sc ii}}$ $\lambda\lambda$8498,8542', size=24)
 ax.axvline(x=8498, color='0.75')
 ax.axvline(x=8542, color='0.75')
-#ax.xaxis.set_major_locator(MaxNLocator(4))
+ax.xaxis.set_major_locator(MaxNLocator(4))
 plot_chunk(f1, diff, 8490, 8550, red)
 
-# Star 2
-#diff = f2 - fm
-diff = (fm - f2)/f2
-plt.figure(2, figsize=(18,8))
-ax = plt.subplot2grid((2,5), (1,2), colspan=2)
-ax.get_yaxis().set_ticklabels([])
-plt.title('$\hbox{Ca\kern 0.1em{\sc ii}}$ $\lambda\lambda$8498,8542', size=24)
-ax.axvline(x=8498, color='0.75')
-ax.axvline(x=8542, color='0.75')
-#ax.xaxis.set_major_locator(MaxNLocator(4))
-plot_chunk(f2, diff, 8493, 8545, yel)
+## Star 2
+##diff = f2 - fm
+#diff = (fm - f2)/f2
+#plt.figure(2, figsize=(18,8))
+#ax = plt.subplot2grid((2,5), (1,2), colspan=2)
+#ax.get_yaxis().set_ticklabels([])
+#plt.title('$\hbox{Ca\kern 0.1em{\sc ii}}$ $\lambda\lambda$8498,8542', size=24)
+#ax.axvline(x=8498, color='0.75')
+#ax.axvline(x=8542, color='0.75')
+##ax.xaxis.set_major_locator(MaxNLocator(4))
+#plot_chunk(f2, diff, 8493, 8545, yel)
 
 plt.show()
 
